6be27905ecabd6c04dc14ac49b67636f770bfadb
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2026-04-20 14:17:47 -0300

    Tipos de dados - string
    Conhecendo o tipo de dado String(texto)

---
+++ aula003.py
@@ -0,0 +1,24 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> String -> Texto
+Strings são textos que estão dentro das aspas
+"""
+
+print(1234)
+
+# Aspas simples
+
+print('Nelson Santos')
+print('Nelson "Santos"')
+# Aspas duplas
+
+print("Nelson Santos")
+print("Nelson 'Santos'")
+# Escape
+
+print("Nelson \"Santos\"")
+
+# r
+
+print(r"Nelson \"Santos\"")
