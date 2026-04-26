e9f002466b7975bd6e9a5a5a340aaa35216ef4b4
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2026-04-26 11:03:23 -0300

    Variáveis
    Aula sobre variáveis

---
+++ aula007.py
@@ -0,0 +1,13 @@
+"""
+VARIÁVEIS
+
+Variáveis são usadas para salvar algo na memória do computador
+PEP8: Inicie variáveis com letras minúsculas, pode usar
+números e undeline_.
+Osunal de = é o operador de atribuição. Ele é usado para
+atribuir um valor a um nome (variável).
+Uso: nome_variavel = expressão
+"""
+nome_completo = 'Nelson José do Santos Júnior'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
