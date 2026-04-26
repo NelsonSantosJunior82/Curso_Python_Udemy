d4575434992ba74934d1b8d6f194c9717759721d
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2026-04-26 20:25:10 -0300

    Exercício de variáveis

---
+++ aula008.py
@@ -0,0 +1,13 @@
+nome = 'Nelson'
+sobrenome = 'Santos Júnior'
+idade = 44
+ano_nascimento = 2026 - idade
+maior_de_idade = idade >= 18
+altura_metro = 1.67
+
+print('Nome: ',nome)
+print('Sobrenome: ',sobrenome)
+print('Idade: ',idade)
+print('Ano de nascimento: ',ano_nascimento)
+print('É maior de idade? ',maior_de_idade)
+print('Altura em metros: ',altura_metro)
