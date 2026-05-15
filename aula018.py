f38196a1321f421aca06699c802346da6d66c5d0
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2026-05-15 17:28:03 -0300

    Interpretador e deubugger

---
+++ aula018.py
@@ -0,0 +1,22 @@
+# if / elif      /else
+# se / se não se / se não
+
+condicao1 = True
+condicao2 = True
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código pra condição 1!')
+
+elif condicao2:
+    print('Código pra condição 2!')
+
+elif condicao3:
+    print('Código pra condição 1!') 
+
+elif condicao4:
+    print('Código pra condição 1!')
+
+else:
+    print('')
