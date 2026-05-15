2810cc2e8cede2811ed35f2e8917baf4e4ce9f69
NelsonSantosJunior82 <nelsonjunior13@hotmail.com>
2026-05-15 17:27:47 -0300

    Condicionais pt2

---
+++ aula017.py
@@ -0,0 +1,16 @@
+# if / elif      /else
+# se / se não se / se não
+
+
+
+
+condicao = False
+
+if condicao:
+    print('Esse é o código if!')
+else:
+    print('Este é o else do primeiro if!')
+
+if 10 == 10:
+    print('Outro if!')
+print('FORA DO IF!')
